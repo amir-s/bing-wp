16a554920f56f0a1ee038827da8557c7efbf5a43
Amir Saboury <amir.saboury@shopify.com>
2019-06-16 16:57:38 -0400

    configure now

--- utils.js
+++ utils.js
@@ -34,7 +34,38 @@ function parseDescription(description) {
   };
 }
 
+async function getStock(symbol, market = 'nyse') {
+  const r = await fetch(`https://www.google.com/search?q=${symbol}+${market}`);
+  const d = await r.text();
+  const $ = cheerio.load(d);
+  const rawPrice = $('span:contains("Stock Price")')
+    .parent()
+    .next()
+    .next()
+    .text();
+
+  const priceRegex = /(\d+\.?\d*) ([-\+]\d+\.?\d*) \((\d+\.?\d*)%\)/;
+  const priceData = priceRegex.exec(rawPrice);
+  if (!priceData) return null;
+
+  const rawCurrency = $('span:contains("Currency in ") span').text();
+  const currencyRegex = /Currency in ([^ ]*)/;
+  const currencyData = currencyRegex.exec(rawCurrency);
+
+  const currency = currencyData && currencyData[1];
+
+  return {
+    symbol: symbol.toUpperCase(),
+    market: market.toUpperCase(),
+    price: priceData[1],
+    change: priceData[2],
+    changePercentage: priceData[3],
+    currency,
+  };
+}
+
 module.exports = {
   getImage,
   parseDescription,
+  getStock,
 };
