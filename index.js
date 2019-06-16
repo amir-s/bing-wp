bff3510b4b270198d582e1021bbfc6358b4b57c2
Amir Saboury <amir.saboury@shopify.com>
2019-06-16 17:30:00 -0400

    v1

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ const photos = require('./db/parsed');
 
 const { getImage } = require('./utils');
 
-const PORT = process.env.PORT || 8080;
-
 const app = new Koa();
 var router = new Router();
 
-router.get('/random', ctx => {
+router.get('/v1/photo/random', ctx => {
   const index = Math.floor(Math.random() * photos.length);
   ctx.body = photos[index];
 });
 
-router.get('/:index?', async ctx => {
+router.get('/v1/photo/:index?', async ctx => {
   ctx.body = await getImage(~~ctx.params.index);
+});
+
+router.get('/', ctx => {
+  ctx.body = 'Hello!';
 });
 
 app
